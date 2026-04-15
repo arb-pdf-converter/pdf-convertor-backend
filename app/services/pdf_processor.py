099cb32c3df049d603ca546c6ea97eac462b1846
arb-pdf-converter <mahmodabusableh@gmail.com>
2026-04-15 15:14:04 +0300

    Update pdf_processor.py

--- app/services/pdf_processor.py
+++ app/services/pdf_processor.py
@@ -15,16 +15,21 @@ class PDFProcessor:
         pdf_bytes = img2pdf.convert(images)
         return pdf_bytes
     
+
+
     @staticmethod
     def merge_pdfs(pdf_bytes_list: list[bytes]) -> bytes:
-        """Merge PDFs using PyPDF2"""
-        merger = PdfMerger()
+        writer = PdfWriter()
+
         for pdf_bytes in pdf_bytes_list:
-            merger.append(BytesIO(pdf_bytes))
+            reader = PdfReader(BytesIO(pdf_bytes))
+            for page in reader.pages:
+                writer.add_page(page)
+
         output = BytesIO()
-        merger.write(output)
-        merger.close()
+        writer.write(output)
         output.seek(0)
+
         return output.read()
     
     @staticmethod
